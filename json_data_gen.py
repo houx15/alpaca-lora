d83124dedf79fd53e8b0ab9c3ba6d6ea6ee1380d
houyuxin <houyuxin1234@sina.cn>
2023-08-06 17:24:09 +0800

    data gen

---
+++ json_data_gen.py
@@ -0,0 +1,89 @@
+import argparse
+import os
+import pandas as pd
+import json
+
+
+def sentence_cleaner(sentence:str):
+    import re
+    sentence = sentence.replace("\"", "")
+    sentence = sentence.replace("RT", "")
+    sentence = sentence.replace(".", "")
+    sentence = sentence.replace("\'", "")
+    results = re.compile(r'[http|https]*://[a-zA-Z0-9.?/&=:_%,-~]*', re.S)
+    sentence = re.sub(results, '', sentence)
+    sentence = re.sub('[\u4e00-\u9fa5]', '', sentence)
+    # results2 = re.compile(r'[@].*?[ ]', re.S)
+    # sentence = re.sub(results2, '', sentence)
+    sentence = sentence.replace("\n", " ")
+    sentence = sentence.strip()
+    results2 = re.compile(r'[@].*?[ ]', re.S)
+    sentence = re.sub(results2, '', sentence)
+    return sentence
+
+
+class DataGen(object):
+    def __init__(self, topic, output_dir) -> None:
+        self.topic = topic
+        self.translator = self.load_result_translator()
+        self.load_dataset()
+        self.prompt = self.load_prompt_designer()
+
+        self.export_json_data(self.train_df, f"{output_dir}/train.json")
+        self.export_json_data(self.val_df, f"{output_dir}/val.json")
+        self.export_json_data(self.test_df, f"{output_dir}/test.json")
+
+    def load_dataset(self) -> None:
+        data_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), f'dataset/{self.topic}')
+        self.train_df = pd.read_csv(os.path.join(data_dir, 'train-regression.csv'), header=0, usecols=['text', 'label'])
+        self.test_df = pd.read_csv(os.path.join(data_dir, 'test-regression.csv'), header=0, usecols=['text', 'label'])
+        self.val_df = pd.read_csv(os.path.join(data_dir, 'validate-regression.csv'), header=0, usecols=['text', 'label'])
+
+        # self.df = pd.read_csv(os.path.join(data_dir, 'total-regression.csv'), header=0, usecols=['text', 'label'])
+
+        # prompt based
+        convert_label_dict = {}
+        for k, v in self.translator['labels'].items():
+            convert_label_dict[v] = k
+        
+        self.train_df['label'] = self.train_df['label'].map(convert_label_dict)
+        self.val_df['label'] = self.val_df['label'].map(convert_label_dict)
+        self.test_df['label'] = self.test_df['label'].map(convert_label_dict)
+        # self.df['label'] = self.df['label'].map(convert_label_dict)
+
+    def load_result_translator(self) -> dict:
+        with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'configs/label_map.json'), 'r', encoding='utf8') as rfile:
+            translator_dict = json.loads(rfile.read())
+            return translator_dict[self.topic]
+    
+    def load_prompt_designer(self) -> str:
+        with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'configs/prompt_design.json'), 'r', encoding='utf8') as rfile:
+            prompt_dict = json.loads(rfile.read())
+            return prompt_dict[self.topic]
+    
+
+    def export_json_data(self, df: pd.DataFrame, output: str) -> None:
+        res = []
+        for index, row in df.iterrows():
+            res.append({
+                "instruction": self.prompt,
+                "input": sentence_cleaner(row["text"]),
+                "output": row["label"]
+            })
+        
+        with open(output, "w", encoding="utf8") as wfile:
+            json.dump(res, output)
+
+
+if __name__ == "__main__":
+    # parser = argparse.ArgumentParser(description='Prediction/Evaluation.')
+    
+    # parser.add_argument('topic', type=str, choices=['gun', 'china', 'abortion', 'drug', 'climate', 'sexual'])
+    # parser.add_argument('--output_dir', type=str, default='./data')
+
+    # config = parser.parse_args()
+
+    # evaluator = DataGen(topic=config.topic, output_dir=config.output_dir)
+
+    for topic in ['gun', 'china', 'abortion', 'drug', 'climate', 'sexual']:
+        generator = DataGen(topic=topic, output_dir=f'json/{topic}')
